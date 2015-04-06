34888eb7bd34334eb5159eb70e201fb96031d13b
Hack Reactor Students <accounts@hackreactor.com>
2015-04-06 16:00:18 -0700

    Still testing gitignore

--- client/env/config.js
+++ client/env/config.js
@@ -12,6 +12,6 @@ if (!/(&|\?)username=/.test(window.location.search)) {
 $.ajaxPrefilter(function (settings, _, jqXHR) {
 //  jqXHR.setRequestHeader("X-Parse-Application-Id", "PARSE_APP_ID");
 //  jqXHR.setRequestHeader("X-Parse-REST-API-Key", "PARSE_API_KEY");
-  jqXHR.setRequestHeader("X-Parse-Application-Id", "asdf");
-  jqXHR.setRequestHeader("X-Parse-REST-API-Key", "asdfsdf");
+  jqXHR.setRequestHeader("X-Parse-Application-Id", "SDFEFasdf");
+  jqXHR.setRequestHeader("X-Parse-REST-API-Key", "TEfewasdfsdf");
 });
