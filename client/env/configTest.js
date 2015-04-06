df741f1c3aa7651c419fc9bdc8b3cbb7bc099106
Michael Harris <mwharris333@gmail.com>
2015-04-06 13:11:17 -0700

    Testing config.js and configTest.js with .gitignore file

--- client/env/configTest.js
+++ client/env/configTest.js
@@ -10,6 +10,8 @@ if (!/(&|\?)username=/.test(window.location.search)) {
 
 // Put your parse application keys here!
 $.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader("X-Parse-Application-Id", "PARSE_APP_ID");
-  jqXHR.setRequestHeader("X-Parse-REST-API-Key", "PARSE_API_KEY");
+//  jqXHR.setRequestHeader("X-Parse-Application-Id", "PARSE_APP_ID");
+//  jqXHR.setRequestHeader("X-Parse-REST-API-Key", "PARSE_API_KEY");
+  jqXHR.setRequestHeader("X-Parse-Application-Id", "...id");
+  jqXHR.setRequestHeader("X-Parse-REST-API-Key", "...key");
 });
